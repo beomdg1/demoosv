a9936f35edf1cde088b29568f6411fb214160746
beomdg1 <nguyenhoan08092017@gmail.com>
2026-05-28 08:33:54 +0700

    fix static folder

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'app')))
 
 const PORT = process.env.PORT || 3000
 
